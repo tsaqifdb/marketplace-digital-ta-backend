9eb6fad600f88c266a33f0f4637ba3569cc1241b
tsaqifdb <tsaqifworkspace@gmail.com>
2025-01-12 12:50:35 +0700

    update eror koneksi

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"context"
 	"log"
 
-	"github.com/gin-contrib/cors" // Tambahkan ini
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -48,6 +48,13 @@ func main() {
 			auth.POST("/login", authHandler.Login)
 		}
 	}
+
+	// Test route
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
 
 	// Start server
 	if err := r.Run(":8080"); err != nil {
